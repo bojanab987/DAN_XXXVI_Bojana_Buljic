f3bf0c19158043466216cec70e14212c58e15600
bojanabb <bojana.buljic@gmail.com>
2020-06-22 15:28:44 +0200

    Implemented CreateMatrix and GenerateNumbers methods

--- DAN_XXXVI_Bojana_Buljic/Zadatak_1/Program.cs
+++ DAN_XXXVI_Bojana_Buljic/Zadatak_1/Program.cs
@@ -2,12 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
     class Program
     {
+        static int[,] matrix;
+        static List<int> list = new List<int>(10000);
+        //object to lock for Creating and filling matrix
+        static readonly object locker = new object();
+        //object to lock for writing/reading file
+        static readonly object fileLocker = new object();
+        //file path
+        static string file = @"../../File.txt";
+        static int[] oddNumbers;
+
+        /// <summary>
+        /// Method for creating 100x100 matrix
+        /// </summary>
+        /// <returns></returns>
+        static void CreateMatrix()
+        {
+            lock (locker)
+            {
+                matrix = new int[100, 100];
+
+                while (list.Count < 10000)
+                {
+                    Monitor.Wait(locker);
+                }
+
+                for (int i = 0; i < 100; i++)
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+
+                        matrix[i, j] = list[j + i * 100];
+
+                    }
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// Method is Generating random numbers and adding it into list
+        /// </summary>
+        static void GenerateNumbers()
+        {
+            int number;
+            Random rnd = new Random();
+            lock (locker)
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    number = rnd.Next(10, 100);
+                    list.Add(number);
+                }
+                Monitor.Pulse(locker);
+            }
+
+        }
+
         static void Main(string[] args)
         {
         }
